1a96a8d47d03766413d804197470fd3005c25301
borict <tamara.boric8@gmail.com>
2022-12-06 17:11:00 +0100

    Zadatak 5

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,4 +2,7 @@
 <div class="blog-post">
     <h2 class="blog-post-title">{{ $movie->title }}</h2>
         <a href="/users/{{ $movie->title }}"></a>
+        <p>Genre: {{$movie->genre}}</p>
+        <p>Director: {{$movie->director}}</p>
+        <p>Storyline: {{$movie->storyline}}</p>
     </p>
