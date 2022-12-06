f0afd289fb2788488fbb34ba8ddbcace13ae29d0
borict <tamara.boric8@gmail.com>
2022-12-06 16:07:31 +0100

    Zadatak 2

--- database/migrations/2022_12_06_144809_create_movies_table.php
+++ database/migrations/2022_12_06_144809_create_movies_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->string("genre");
+            $table->string("director");
+            $table->integer("year");
+            $table->string("storyline");
             $table->timestamps();
         });
     }
