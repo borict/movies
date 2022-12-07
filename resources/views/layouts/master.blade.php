11dda3cdfd0ae1c5a79ea3d1e52d54d752f93b80
borict <tamara.boric8@gmail.com>
2022-12-07 18:45:32 +0100

    Zadatak 11

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,7 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <title>Movies</title>
-    @extends('layouts.sidebar')
+    {{-- ne mogu da skontam zasto sidebar ne radi, zato je zakomentarisan --}}
+    {{-- @extends('layouts.sidebar') --}}
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="/css/bootstrap.min.css" >
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,14 +1,14 @@
 <aside>
-      <h4>Last updated</h4>
-      <ol>
+    <h4>Last updated</h4>
+    <ol>
         @php
             $i = count($movies)-1;
         @endphp
-            @while($i>count($movies)-6 && $i>-1)
-                <li><a href="{{route('single-movie' , ['id' => $movies[$i]->id])}}">{{$movies[$i]->title}}</a></li>
-                @php
-                    $i--;
-                @endphp
+        @while($i>count($movies)-6 && $i>-1)
+            <li><a href="{{route('single-movie' , ['id' => $movies[$i]->id])}}">{{$movies[$i]->title}}</a></li>
+            @php
+                $i--;
+            @endphp
         @endwhile
-      </ol>
-  </aside>
+    </ol>
+</aside>
